3c7d81ddcdbd17127f763d117d967915c61f13ea
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-03-14 13:02:53 +0100

    modifying Index page controller

--- src/main/java/com/gabriel/springrecipe/controllers/IndexController.java
+++ src/main/java/com/gabriel/springrecipe/controllers/IndexController.java
@@ -1,13 +1,16 @@
 package com.gabriel.springrecipe.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "index", "index.html"})
-    public String getIndexPage() {
+    public String getIndexPage(Model model) {
+        String text = "milk is good and hot ...";
+        model.addAttribute("recipe", text);
         return "index";
     }
 
