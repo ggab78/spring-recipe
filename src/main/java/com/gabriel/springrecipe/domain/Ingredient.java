8c5cea0f3c4ac2168c111c7d0cd8c28b718684ce
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-03-23 17:46:53 +0100

    Create UnitOfMeasure for Ingredient and unidirectional onetoone mapping. Closes #2.

--- src/main/java/com/gabriel/springrecipe/domain/Ingredient.java
+++ src/main/java/com/gabriel/springrecipe/domain/Ingredient.java
@@ -16,8 +16,8 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //todo
-    //private UnitOfMeasure uom;
+    @OneToOne (fetch = FetchType.EAGER)//EAGER is default behaviour for ..toOne mapping
+    private UnitOfMeasure uom;
 
 
     public Long getId() {
@@ -50,5 +50,13 @@ public class Ingredient {
 
     public void setAmount(BigDecimal amount) {
         this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
