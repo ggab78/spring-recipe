50af58440e6269f0a8000103d305793bbdde1ac7
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-04-04 21:49:10 +0200

    adding mockito argument capture test for RecipeController

--- src/test/java/com/gabriel/springrecipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/gabriel/springrecipe/controllers/RecipeControllerTest.java
@@ -1,12 +1,17 @@
 package com.gabriel.springrecipe.controllers;
 
 
+import com.gabriel.springrecipe.domain.Recipe;
 import com.gabriel.springrecipe.services.RecipeService;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
@@ -31,11 +36,26 @@ public class RecipeControllerTest {
     @Test
     public void getRecipeList() {
 
+        //given
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(new Recipe());
+
+        when(recipeService.getRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+
+        //when
         String str = recipeController.getRecipeList(model);
+
+        //then
         assertEquals("recipes/index", str);
 
+
         verify(recipeService, times(1)).getRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        assertEquals(1, argumentCaptor.getValue().size());
 
     }
 }
