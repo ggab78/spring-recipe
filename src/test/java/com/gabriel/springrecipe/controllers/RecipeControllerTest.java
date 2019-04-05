88fe9d11bd53821965ec237213538b232e720708
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-04-05 20:51:37 +0200

    adding mockMVC test for RecipeController

--- src/test/java/com/gabriel/springrecipe/controllers/RecipeControllerTest.java
+++ src/test/java/com/gabriel/springrecipe/controllers/RecipeControllerTest.java
@@ -8,6 +8,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,6 +18,9 @@ import java.util.Set;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 public class RecipeControllerTest {
 
@@ -32,6 +38,15 @@ public class RecipeControllerTest {
         recipeController = new RecipeController(recipeService);
     }
 
+    @Test
+    public void testMockMVC() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(recipeController).build();
+
+        mockMvc.perform(get("/recipes"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipes/index"));
+    }
+
 
     @Test
     public void getRecipeList() {
@@ -41,7 +56,6 @@ public class RecipeControllerTest {
         recipeSet.add(new Recipe());
 
         when(recipeService.getRecipes()).thenReturn(recipeSet);
-
         ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
 
 
