e337d1b9017d9e73cc447e6a82574c8bad13e7ba
Gabriel Gamrat <gabriel.gamrat@gmail.com>
2019-04-13 16:30:08 +0200

    add recipeform.html and RecipeController method to create new recipe

--- src/main/java/com/gabriel/springrecipe/controllers/RecipeController.java
+++ src/main/java/com/gabriel/springrecipe/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package com.gabriel.springrecipe.controllers;
 
+import com.gabriel.springrecipe.commands.RecipeCommand;
 import com.gabriel.springrecipe.services.RecipeService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -22,5 +25,20 @@ public class RecipeController {
         log.debug("getting recipe/show/{id} page");
 
         return "recipes/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("newrecipe", new RecipeCommand());
+        return "recipes/recipeform";
+    }
+
+    @PostMapping
+    @RequestMapping(name="new_recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
